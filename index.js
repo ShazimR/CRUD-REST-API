81ae26a4d5a8c7f7eb802031a302b5069ab90bce
Shazim Rahman <shazimrahman@hotmail.com>
2023-06-25 22:32:33 -0400

    added express server that runs on localhost

--- index.js
+++ index.js
@@ -4,3 +4,18 @@ const PORT = process.env.PORT;
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+
+app.use(bodyParser.json());
+app.use(
+    bodyParser.urlencoded({
+        extended: true,
+    })
+);
+
+app.get('/', (req, res) => {
+    res.json({ info: 'Node.js, Express, and Postgres API' })
+});
+
+app.listen(PORT, () => {
+    console.log(`App running on port: ${PORT}`)
+});
